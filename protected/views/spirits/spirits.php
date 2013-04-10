eb33f179412122d476003c22a3f94df85aea80b8
blakefaris <blakefaris@gmail.com>
2013-04-09 23:46:38 -0500

    Correctly space content.

--- protected/views/spirits/spirits.php
+++ protected/views/spirits/spirits.php
@@ -5,14 +5,14 @@ $this->renderPartial('brews');
 
 
 <script type="text/javascript">
-  // brews - load data object (JSON) into JavaScript variable
-  billy.templateData = <?php echo $brews; ?>;
+  	// brews - load data object (JSON) into JavaScript variable
+  	billy.templateData = <?php echo $brews; ?>;
 
-  // compile template
-  var brewTemplateSource   = $("#brew-template").html();
-  var brewTemplate = Handlebars.compile(brewTemplateSource);
+  	// compile template
+  	var brewTemplateSource   = $("#brew-template").html();
+  	var brewTemplate = Handlebars.compile(brewTemplateSource);
 
-  // render template
-  var brewTemplateHtml = brewTemplate(billy.templateData);
-  $('#content').append(brewTemplateHtml);
+  	// render template
+  	var brewTemplateHtml = brewTemplate(billy.templateData);
+	$('#content').append(brewTemplateHtml);
 </script>
