b6d2008afd9f01f0d0b3657cd3b7177bebaed0b0
blakefaris <blakefaris@gmail.com>
2013-04-09 22:42:00 -0500

    Support multiple brews in the template.

--- protected/views/spirits/spirits.php
+++ protected/views/spirits/spirits.php
@@ -3,6 +3,7 @@
 
 
 <script id="brew-template" type="text/x-handlebars-template">
+{{#each brews}}
   <div class="brew">
     <div class="brew-title">{{title}}</div>
     <div class="brew-type">{{type}}</div>
@@ -22,31 +23,56 @@
 			{{/each}}
     </ul>
   </div>
+{{/each}}
 </script>
 
 
 <script type="text/javascript">
   // brews - load data object (JSON) into JavaScript variable
   billy.templateData = {
-    title: "Double Trouble",
-    type: "IPA",
-    details: {
-      description: "Double Trouble was the first brew by Billy's Cantina, and as such, not much information is available for it.",
-      imageSrc: "http://billyscantina.com/image.php/doubleTrouble.jpeg?width=533&height=533&image=/images/spirits/doubleTrouble.jpeg",
-      additional: [
-        {
-          label: "Keg Status",
-          details: "Empty"
-        }
-      ],
-      reviews: [
-        {
-          title: "Best beer ever!",
-          details: "This was by far the best beer that I have ever had.  It was nice and hoppy"
-        }
-      ]
-    }
-  };
+	  brews: [
+		  {
+		    title: "Double Trouble",
+		    type: "IPA",
+		    details: {
+		      description: "Double Trouble was the first brew by Billy's Cantina, and as such, not much information is available for it.",
+		      imageSrc: "http://billyscantina.com/image.php/doubleTrouble.jpeg?width=533&height=533&image=/images/spirits/doubleTrouble.jpeg",
+		      additional: [
+		        {
+		          label: "Keg Status",
+		          details: "Empty"
+		        }
+		      ],
+		      reviews: [
+		        {
+		          title: "Best beer ever!",
+		          details: "This was by far the best beer that I have ever had.  It was nice and hoppy"
+		        }
+		      ]
+		    }
+		  },
+		  {
+		    title: "Double Trouble",
+		    type: "IPA",
+		    details: {
+		      description: "Double Trouble was the first brew by Billy's Cantina, and as such, not much information is available for it.",
+		      imageSrc: "http://billyscantina.com/image.php/doubleTrouble.jpeg?width=533&height=533&image=/images/spirits/doubleTrouble.jpeg",
+		      additional: [
+		        {
+		          label: "Keg Status",
+		          details: "Empty"
+		        }
+		      ],
+		      reviews: [
+		        {
+		          title: "Best beer ever!",
+		          details: "This was by far the best beer that I have ever had.  It was nice and hoppy"
+		        }
+		      ]
+		    }
+		  }
+	  ]
+};
 
   // compile template
   var brewTemplateSource   = $("#brew-template").html();
