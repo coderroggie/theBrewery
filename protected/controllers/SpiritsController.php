b534f7417b8494c342c8af3532c49ec9dd054888
blakefaris <blakefaris@gmail.com>
2013-04-09 23:43:40 -0500

    Removed incorrect comment.

--- protected/controllers/SpiritsController.php
+++ protected/controllers/SpiritsController.php
@@ -2,10 +2,6 @@
 
 class SpiritsController extends Controller {
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionSpirits() {
 		
 		$brewsFile = new DataFile('brews.json');
