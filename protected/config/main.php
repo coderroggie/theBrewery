1f1071c917ee6f8b88fbaa958b137ccd05fd1ce7
blakefaris <blakefaris@gmail.com>
2013-04-09 21:16:26 -0500

    Use Friendly URLS
    this change is also related to the .htaccess change

--- protected/config/main.php
+++ protected/config/main.php
@@ -50,16 +50,17 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
+				'gii'=>'gii',
+				'gii/<controller:[\w\-]+>'=>'gii/<controller>',
+				'gii/<controller:[\w\-]+>/<action:\w+>'=>'gii/<controller>/<action>',
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
