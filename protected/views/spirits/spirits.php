8e7c31a4ddab3007e769afd6da91206a1752cd3b
blakefaris <blakefaris@gmail.com>
2013-04-09 22:33:35 -0500

    Initial Handlebars template for Spirits
    Data is hardcoded into the view.

--- protected/views/spirits/spirits.php
+++ protected/views/spirits/spirits.php
@@ -1,7 +1,60 @@
-Spirits
 
-// load data object (JSON) into JavaScript variable
 
-// add handlebars template
 
-// render template
+
+<script id="brew-template" type="text/x-handlebars-template">
+  <div class="brew">
+    <div class="brew-title">{{title}}</div>
+    <div class="brew-type">{{type}}</div>
+    <div class="brew-description">{{details.description}}</div>
+    <ul class="brew-additional">
+			{{#each details.additional}}
+				<li>
+					<span class="brew-additional-label">{{label}}</span>
+					<span class="brew-additional-details">{{details}}</span>
+				</li>
+			{{/each}}
+			{{#each details.reviews}}
+				<li>
+					<span class="brew-additional-label">{{title}}</span>
+					<span class="brew-additional-details">{{details}}</span>
+				</li>
+			{{/each}}
+    </ul>
+  </div>
+</script>
+
+
+<script type="text/javascript">
+  // brews - load data object (JSON) into JavaScript variable
+  billy.templateData = {
+    title: "Double Trouble",
+    type: "IPA",
+    details: {
+      description: "Double Trouble was the first brew by Billy's Cantina, and as such, not much information is available for it.",
+      imageSrc: "http://billyscantina.com/image.php/doubleTrouble.jpeg?width=533&height=533&image=/images/spirits/doubleTrouble.jpeg",
+      additional: [
+        {
+          label: "Keg Status",
+          details: "Empty"
+        }
+      ],
+      reviews: [
+        {
+          title: "Best beer ever!",
+          details: "This was by far the best beer that I have ever had.  It was nice and hoppy"
+        }
+      ]
+    }
+  };
+
+  // compile template
+  var brewTemplateSource   = $("#brew-template").html();
+  var brewTemplate = Handlebars.compile(brewTemplateSource);
+
+  // render template
+  var brewTemplateHtml = brewTemplate(billy.templateData);
+  $('#content').append(brewTemplateHtml);
+
+</script>
+
