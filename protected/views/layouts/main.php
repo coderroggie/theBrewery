ef1d7765b9e104900df3fee6362492744b28aa2c
blakefaris <blakefaris@gmail.com>
2013-04-10 18:07:09 -0500

    Use app name in footer's copyright.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -60,9 +60,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		Copyright &copy; <?php echo date('Y').' '.CHtml::encode(Yii::app()->name).' | All Rights Reserved.' ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
